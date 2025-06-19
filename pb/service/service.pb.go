b1ceb0d1d529e2530d092c97fbbb311c49ae73fb
Aldi Nugraha <aldi.nugrahatk@gmail.com>
2025-06-19 16:27:38 +0700

    feat

--- pb/service/service.pb.go
+++ pb/service/service.pb.go
@@ -7,6 +7,7 @@
 package service
 
 import (
+	_ "buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go/buf/validate"
 	common "github.com/aldngrha/ecommerce-be/pb/common"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
@@ -122,9 +123,9 @@ var File_service_service_proto protoreflect.FileDescriptor
 
 const file_service_service_proto_rawDesc = "" +
 	"\n" +
-	"\x15service/service.proto\x12\aservice\x1a\x1acommon/base_response.proto\"\"\n" +
-	"\fHelloRequest\x12\x12\n" +
-	"\x04name\x18\x01 \x01(\tR\x04name\"S\n" +
+	"\x15service/service.proto\x12\aservice\x1a\x1acommon/base_response.proto\x1a\x1bbuf/validate/validate.proto\"-\n" +
+	"\fHelloRequest\x12\x1d\n" +
+	"\x04name\x18\x01 \x01(\tB\t\xbaH\x06r\x04\x10\x01\x18dR\x04name\"S\n" +
 	"\rHelloResponse\x12(\n" +
 	"\x04base\x18\x01 \x01(\v2\x14.common.BaseResponseR\x04base\x12\x18\n" +
 	"\amessage\x18\x02 \x01(\tR\amessage2N\n" +
